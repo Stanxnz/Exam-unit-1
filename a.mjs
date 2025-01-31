dd26c19acb44748f087244931b81b625686b5587
Stanxnz <stanvdkooi@gmail.com>
2025-01-31 17:31:29 +0100

    Done w task A, also tested.

--- a.mjs
+++ a.mjs
@@ -18,10 +18,22 @@ import test from "./test.mjs";
 */
 
 //#region function -----------------------------------------------------------------
-// Write your function her.
+function multiply(...numbers){
+    if (numbers.length === 0) return NaN;
 
+    let product = 1;
 
-
+    for (let number of numbers){
+        if (typeof number === "string" && ! isNaN(number)){
+            number = parseFloat(number);
+        }
+        if (typeof number !== "number" || isNaN(number)){
+            return NaN;
+        }
+        product *= number;
+    }
+    return product;
+}
 //#endregion
 
 
@@ -29,8 +41,22 @@ import test from "./test.mjs";
 
 
 //#region Tests --------------------------------------------------------------------
-// Write your tests her.
+const tests = test("Multiply function");
 
+tests.isEqual(multiply(2, 3), 6, "Multiplication of 2 and 3 should be 6");
+tests.isEqual(multiply(5, 5, 2), 50, "Multiplication of 5, 5, and 2 should be 50");
+tests.isEqual(multiply(1.5, 2), 3, "Multiplication of 1.5 and 2 should be 3");
 
+tests.isEqual(multiply("2", 3), 6, 'Multiplication of "2" and 3 should be 6');
+tests.isEqual(multiply("4", "5"), 20, 'Multiplication of "4" and "5" should be 20');
 
+tests.isNotANumber(multiply("hello", 3), 'Multiplication of "hello" and 3 should return NaN');
+tests.isNotANumber(multiply(2, undefined), "Multiplication of 2 and undefined should return NaN");
+tests.isNotANumber(multiply(null, 3), "Multiplication of null and 3 should return NaN");
+tests.isNotANumber(multiply(), "Multiplication with no arguments should return NaN");
+
+tests.isEqual(multiply(0, 5), 0, "Multiplication of 0 and 5 should be 0");
+tests.isEqual(multiply(1, Infinity), Infinity, "Multiplication of 1 and Infinity should be Infinity");
+tests.isEqual(multiply(-1, Infinity), -Infinity, "Multiplication of -1 and Infinity should be -Infinity");
+tests.isNotANumber(multiply(NaN, 5), "Multiplication of NaN and 5 should return NaN");
 //#endregion
